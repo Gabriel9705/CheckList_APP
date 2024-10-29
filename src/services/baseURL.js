d45030cc6b45fd7110e86b1ef0cf81d25408572b
GuilhermeRS <guilhermemuralha2016@gmail.com>
2024-10-29 08:18:01 -0300

    fix: servidor cloud

--- src/services/baseURL.js
+++ src/services/baseURL.js
@@ -1,4 +1,4 @@
 const serveLocal = "http://localhost:3000";
-const serveClound = "https://checklist-api-zb42.onrender.com"
+const serveCloud = "https://checklist-api-zb42.onrender.com"
 
-export const baseURL = serveLocal;
+export const baseURL = serveCloud;
